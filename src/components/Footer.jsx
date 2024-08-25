64a7222a558e037743b7b7ec671b47894eb9b94a
sourabhkanolkar <sourabhkanolkar17@gmail.com>
2024-08-25 13:47:24 +0530

    1.47

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../styles/footer.css";
+import { Link } from 'react-router-dom';
 
 export default function Footer() {
   return (
@@ -62,7 +63,7 @@ export default function Footer() {
                   <span className='text-contact'><i className="bi bi-telephone footer-icon"></i>+91 9226055797</span>
                   <span className='text-contact'><i className="bi bi-envelope footer-icon"></i>support@osdiadefestavacation.com</span>
                   <span className='text-contact'><i className="bi bi-envelope footer-icon"></i>reservations<br></br>@osdiadefestavacation.com</span>
-                  
+                   <Link to='/payment-page'>PAYMENT</Link>
                 </p>
 
                </div>
